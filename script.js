de4d1ba82c5f01c061cb8bf7d295cb5e3232646a
Bhanu Chaitanya <143734650+chaitanyabhanu@users.noreply.github.com>
2024-08-09 20:16:47 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,112 @@
+// JavaScript--functions  9-8-2024
+//1. A javascript Function is a block of code to design to do perform sone particular tasks
+// 2. A javaScript Function is Executed when Something is Invokes it call it 
+
+
+// 1.. Example  
+
+// function myfunction(p1,p2){
+//     return p1 *p2;
+// }
+
+// let result = myfunction (4,3);
+
+// document.getElementById("demo").innerHTML = result;
+
+
+
+// 2....Example :--
+
+// function myfunction(a,b){
+//     return(a*b);
+// }
+
+// let result = myfunction(9,9);
+
+// document.getElementById("demo").innerHTML = result;
+
+
+//    3.... Example:--
+    
+//   function myfunction(c,d){
+//     return(c*d);
+// }
+//    let result =myfunction(5,5);
+
+//     document.getElementById("demo").innerHTML = result;
+
+
+//   4...Example:--
+
+// function myfunction(a,b){
+//     return(a*b);
+// }
+
+// let result = myfunction(5,5);
+
+// document.getElementById("demo").innerHTML=result;
+
+
+// 5..example:--
+
+// function myfunction(a,b){
+//     return(a+b);
+// }
+
+// let  result = myfunction(5,5);
+
+// document.getElementById("demo").innerHTML=result;
+
+//   6--Example:--
+
+// function myfunction(a,b){
+//     return(a-b);
+// }
+
+// let result = myfunction (6,4);
+// document.getElementById("demo").innerHTML=result;
+
+//    7 --Example:--
+
+// function myfunction(a,b){
+//     return(a*b);
+// }
+
+// let result = myfunction(9,10);
+// document.getElementById("demo").innerHTML=result;
+
+//   Example--8:--
+
+// function myfunction(a,b){
+//     return(a+b);
+// }
+// let result = myfunction(10,10);
+
+// document.getElementById("demo").innerHTML=result;
+
+
+
+//  Example:--9..
+
+// function myfunction(a,b){
+//     return(a%b);
+// }
+// let result = myfunction(11,10);
+
+// document.getElementById("demo").innerHTML=result;
+
+
+// Example :10---
+
+function myfunction(a,b){
+    return(a*b);
+}
+
+let result = myfunction (15,25);
+document.getElementById("demo").innerHTML=result;
+
+
+
+document.getElementById("style").style.color="blue";
+
+document.getElementById("myH1").style.color = "red";
